6320a051c60a4094cdfe6daecc6eb950287302d1
sagarssc97 <sagar.chauhan@fraazo.com>
2023-01-26 21:58:53 +0530

    footer added

--- src/components/defaults/footer.js
+++ src/components/defaults/footer.js
@@ -2,19 +2,28 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const titles = ["Courses", "About Us", "Blog", "Contact" ]
+
+const social_images = [
+  { img: "/facebook.png"},
+  { img: "/instagram.png"},
+  { img: "/youtube.png"},
+];
+
 export default class Footer extends Component {
     render() {
         return (
           <nav className="footer">
+            <hr style={{borderTop:"20px solid #5C5889", boxShadow:"0 1rem 4px 0 rgb(0 0 0 / 64%)"}}/>
             <div className="nav-container">
-              <div className="logo">
-                ALFA
+              <div className="social-media" style={{paddingTop:"1rem", width:"20%",justifyContent:"space-around", display:"flex"}}>
+              {social_images.map((social, index) =>(
+                <div>
+              <img src={social.img} style={{width:"30px"}}/>
+                </div>
+            ))}
               </div>
-              <div className="nav-menu">
-                {titles.map((title, index) => (  
-                <p className="menu-text">{title}</p>
-                ))}
-              </div>
+              <text style={{color:"#000", position:"absolute", bottom:".2rem", left:"4rem"}}>Copyright © 2023, India Yoga TTC</text>
+
             </div>
           </nav>
         );
