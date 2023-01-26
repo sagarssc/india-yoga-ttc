8c6ccda530842d14222a447b1ec6c0b09b6f908c
sagarssc97 <sagar.chauhan@fraazo.com>
2023-01-26 12:10:25 +0530

    Updates

---
+++ precache-manifest.fab30d954c6410c2fcafedb40840462b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ba38d370ff76bc2b2ef4003621161a",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "9e75b5e8e23d3cc72692",
+    "url": "/myapp/static/css/2.a31e845b.chunk.css"
+  },
+  {
+    "revision": "86efaad3086b90349b54",
+    "url": "/myapp/static/css/main.14707e00.chunk.css"
+  },
+  {
+    "revision": "9e75b5e8e23d3cc72692",
+    "url": "/myapp/static/js/2.fe18b9ac.chunk.js"
+  },
+  {
+    "revision": "86efaad3086b90349b54",
+    "url": "/myapp/static/js/main.31e2dc54.chunk.js"
+  },
+  {
+    "revision": "a5bdc5faa9778bec5c2f",
+    "url": "/myapp/static/js/runtime-main.2348759b.js"
+  }
+]);
